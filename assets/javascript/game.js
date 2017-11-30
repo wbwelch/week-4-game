c26b273faa89d0c7b34d4d141ababcbeb7931deb
wbwelch <wbwelch@icloud.com>
2017-11-30 13:03:17 -0600

    on click events created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,14 +12,24 @@ $(document).ready(function() {
 	var green = 0;
 	var purpleOrange = 0;
 	var randNumber = 0;
+	//var multiCry = $("multi-cry");
+	//var blueCry = $("blue-cry");
+	//var greenCry = $("green-cry");
+	//var purpOrangeCry = $("purple-cry");
 	
 	var playerInteract = {
 		wins: 0,
 		losses: 0,
-		totalScore: 31,
+		totalScore: 91,
 		messages: ["You win!", "You lose!"],
 
+		gameNumber: function () {
+			return Math.floor(Math.random() * (120 - 18 + 1)) + 18;
+		},
 		
+		crystalNumber: function () {
+			return Math.floor(Math.random() * (12 - 1+ 1)) + 1;
+		},
 		scoreCheck: function() {
 			if (this.totalScore === randNumber) {
 				//display you win message
@@ -27,6 +37,9 @@ $(document).ready(function() {
 				//++1 to wins
 				++this.wins;
 				//reset game (new rand number and rand values on crystals)
+				randNumber = this.gameNumber();
+				//console.log(randNumber);
+				
 			}
 			else if (this.totalScore > randNumber) {
 				//display you lose message
@@ -34,19 +47,19 @@ $(document).ready(function() {
 				//++1 to losses
 				++this.losses;
 				//reset game (new rand number and rand values on crystals)
+				randNumber = this.gameNumber();
+				//console.log(randNumber);
+				multi = playerInteract.crystalNumber();
+				blue = playerInteract.crystalNumber();
+				green = playerInteract.crystalNumber();
+				purpleOrange = playerInteract.crystalNumber();
+	
+				console.log(multi);
+				console.log(blue);
+				console.log(green);
+				console.log(purpleOrange);
 			}
-			//else {
-				//do nothing
-			//}
-		},
-		
-		gameNumber: function () {
-			return Math.floor(Math.random() * (120 - 18 + 1)) + 18;
-		},
-		
-		crystalNumber: function () {
-			return Math.floor(Math.random() * (12 - 1+ 1)) + 1;
-		}
+		}	//end of scorecheck method
 	};
 	//end of object
 	
@@ -76,9 +89,30 @@ $(document).ready(function() {
 	
 	//assign crystal numbers to images
 	
+	$("#multi-cry").text(multi);
+	$("#blue-cry").html(blue);
+	$("#green-cry").html(green);
+	$("#purple-cry").html(purpleOrange);
+	
 	//create on click event handler
 	
+	$( "#multi-cry" ).on( "click", function() {
+		console.log("test");
+	});
+	$( "#blue-cry" ).on( "click", function() {
+		console.log("test1");
+	});
+	$( "#green-cry" ).on( "click", function() {
+		console.log("test2");
+	});
+	$( "#purple-cry" ).on( "click", function() {
+		console.log("test3");
+	});
+	
 	//add crystal number click to total score
+	
+	
+	
 	
 	playerInteract.scoreCheck();
 	
