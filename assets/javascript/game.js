a501fa52ffd7df352c1130deb5102267796e9147
wbwelch <wbwelch@icloud.com>
2017-11-30 12:28:46 -0600

    Crystal random number function created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -42,6 +42,10 @@ $(document).ready(function() {
 		
 		gameNumber: function () {
 			return Math.floor(Math.random() * (120 - 18 + 1)) + 18;
+		},
+		
+		crystalNumber: function () {
+			return Math.floor(Math.random() * (12 - 1+ 1)) + 1;
 		}
 	};
 	
@@ -57,14 +61,23 @@ $(document).ready(function() {
 	
 	randNumber = playerInteract.gameNumber();
 	
-	console.log(randNumber)
+	console.log(randNumber);
 	
 	//generate a random number between 1 and 12
 	//creat a for loop to assign random number between 1 and 12 to each image
 	
-	Math.floor(Math.random() * 12) + 1  ;
+	multi = playerInteract.crystalNumber();
+	blue = playerInteract.crystalNumber();
+	green = playerInteract.crystalNumber();
+	purpleOrange = playerInteract.crystalNumber();
+	
+	console.log(multi);
+	console.log(blue);
+	console.log(green);
+	console.log(purpleOrange);
 	
 	//create on click event handler
+	
 	//add random number click to total score
 	
 	
