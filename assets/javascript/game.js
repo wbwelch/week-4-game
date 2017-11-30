c160a25e39250bf3572992b44b843bcf5b2d7444
wbwelch <wbwelch@icloud.com>
2017-11-30 00:11:45 -0600

    Player object created, wins, losses, total score added to screen with jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,4 +5,28 @@ $(document).ready(function() {
 	//$('#losses').text('hi')
 	//$('#score').text('hi')
 	//$('#random-number').text('hi')
+	//$('#win-lose-message').text('hi')
+	
+	var playerInteract = {
+		wins: 0,
+		losses: 0,
+		totalScore: 0,
+		winMes: "You win!",
+		loseMes: "You lose!",
+	}
+	
+	$("#wins").text(playerInteract.wins);
+						//console.log(playerInteract.wins)
+	$("#losses").text(playerInteract.losses);
+	$("#score").text(playerInteract.totalScore);
+
+	
+	
+	//generate a random number between 18 and 120 
+	
+	//generate a random number between 1 and 12
+	//creat a for loop to assign random number between 1 and 12 to each image
+	//create on click event handler
+	//add random number click to total score
+	
 });
