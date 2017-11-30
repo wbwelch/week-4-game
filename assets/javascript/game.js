9d0ed423c031bb61d091dfc3c90e287acd539044
wbwelch <wbwelch@icloud.com>
2017-11-30 11:56:25 -0600

    Score scheck method created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,16 +10,39 @@ $(document).ready(function() {
 	var playerInteract = {
 		wins: 0,
 		losses: 0,
-		totalScore: 0,
+		totalScore: 51,
 		winMes: "You win!",
 		loseMes: "You lose!",
-	}
+		randNumber: 51,
+		
+		scoreCheck: function() {
+			if (this.totalScore === this.randNumber) {
+				//display you win message
+				$("#win-lose-message").text(this.winMes);
+				//++1 to wins
+				++this.wins;
+				//reset game
+			}
+			else if (this.totalScore > this.randNumber) {
+				//display you lose message
+				//++1 to losses
+				//reset game
+			}
+			else {
+				//do nothing
+			}
+		}
+	};
 	
 	$("#wins").text(playerInteract.wins);
 						//console.log(playerInteract.wins)
 	$("#losses").text(playerInteract.losses);
 	$("#score").text(playerInteract.totalScore);
-
+	playerInteract.scoreCheck();
+	
+	//console tests
+	console.log(playerInteract.wins);
+	
 	
 	
 	//generate a random number between 18 and 120 
